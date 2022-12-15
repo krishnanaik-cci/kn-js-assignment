694093745a7172c9c950d9a815a81f196050455e
knaik-cci <krishna.naik@creativecapsule.com>
2022-12-15 18:35:58 +0530

    kn-js-assingment *Optimized the array validation code *Testing and bug fixing of array fields - In Progress

--- JS - Assignment Task/content/scripts/validations/validate.js
+++ JS - Assignment Task/content/scripts/validations/validate.js
@@ -1,6 +1,5 @@
 // Gloabl validation start
 const globalValidation = (numberInput, onlyStringInput, numArrayInput) => {
-  
   //Accept only numeric input
   for (var i = 0; i < numberInput.length; i++) {
     numberInput[i].addEventListener(
@@ -31,30 +30,37 @@ const globalValidation = (numberInput, onlyStringInput, numArrayInput) => {
     numArrayInput[i].addEventListener(
       "input",
       function () {
-        // var oldVal = this.value.replace(/\[/g,'');
-        // oldVal = oldVal.replace(/\]/g,'').replace(/(,{2,})/g, ",");
-        // if(this.value != "[]"){
-        // this.value = '[' + oldVal ;
-        // }else{
-        //   console.log
-        //   this.value = oldVal;
-        // }
-       
-        // if (this.value.length === 1 && this.value === ",") {
-        //   this.value = "";
-        // } else if (this.value.length === 1 && !this.value.match(/(\[)/g)) {
-        //   this.value = "[" + this.value;
-        // } else if (this.value.match(/(,{2,})/g)) {
-        //   this.value = this.value.replace(/(,{2,})/g, ",");
-        // } else if (this.value.match(/(]{2,})/g)) {
-        //   this.value = this.value.replace(/(]{2,})/g, "]");
-        // } else if (this.value.match(/,]/g)) {
-        //   this.value = this.value.replace(/,]/g, "]");
-        // } else if (this.value.match(/],/g)) {
-        //   this.value = this.value.replace(/],/g, "");
-        // } else if (this.value.match(/\[,/g)) {
-        //   this.value = this.value.replace(/\[,/g, "");
-        // }
+
+        this.value = this.value.replace(/(,{2,})/g, ",");
+        
+        if (!this.value.charAt(this.value.length - 1).match(/(\])/g)) {
+          this.value[this.value.length - 1] = "]";
+        }
+
+        if (this.value.charAt(0) == ",") {
+          this.value = "";
+        }
+
+        if (
+          this.value.charAt(0) != "[" ||
+          this.value.charAt(0).match(/(\[)/g) == null ||
+          this.value.charAt(this.value.length - 1) != "]" ||
+          this.value.match(/,]/g) ||
+          this.value.indexOf("[") != this.value.lastIndexOf("[") ||
+          this.value.indexOf("]") != this.value.lastIndexOf("]")
+        ) {
+          this.classList.add("invalid");
+          document
+            .getElementById(this.getAttribute("id") + "Error")
+            .classList.add("d-block");
+        } else {
+          this.classList.remove("invalid");
+          this.classList.remove("d-block");
+          document
+            .getElementById(this.getAttribute("id") + "Error")
+            .classList.remove("d-block");
+        }
+      
 
         this.value = this.value.replace(/[^,\[\]0-9]/g, "");
         enableBtn(document.getElementById(this.getAttribute("data-target")));
@@ -64,67 +70,57 @@ const globalValidation = (numberInput, onlyStringInput, numArrayInput) => {
     numArrayInput[i].addEventListener("paste", (e) => e.preventDefault());
   }
 
-  //onblur check for empty numeric array
-  for (let i = 0; i < numArrayInput.length; i++) {
-    numArrayInput[i].addEventListener(
-      "blur",
-      function () {
-        console.log("this.value.length = "+ this.value == "[]");
-        if (this.value.match(/,]/g) || this.value == "[]") {
-          this.classList.add("invalid");
-          document.getElementById(this.getAttribute("id")+"Error").classList.add("d-block");
-        }else{
-          this.classList.remove("invalid");
-          this.classList.remove("d-block");
-          document.getElementById(this.getAttribute("id")+"Error").classList.remove("d-block");
-        }
-      });
-  }
+  //onblue check for empty numeric array
+  // for (let i = 0; i < numArrayInput.length; i++) {
+  //   numArrayInput[i].addEventListener(
+  //     "blur",
+  //     function () {
+  //       console.log("this.value.length = "+ this.value == "[]");
+  //       if (this.value.match(/,]/g) || this.value == "[]") {
+  //         this.classList.add("invalid");
+  //         document.getElementById(this.getAttribute("id")+"Error").classList.add("d-block");
+  //       }else{
+  //         this.classList.remove("invalid");
+  //         this.classList.remove("d-block");
+  //         document.getElementById(this.getAttribute("id")+"Error").classList.remove("d-block");
+  //       }
+  //     });
+  // }
 
   // Enable button if field is not empty
   const enableBtn = (parentID) => {
     parentID.querySelector("button").removeAttribute("disabled");
-    console.log("parentID = "+ parentID);
     const onlyInputs = parentID.querySelectorAll(".form-control");
     let isEmpty;
     let isArrayEmpty;
 
     onlyInputs.forEach((input) => {
-      //console.log(input.value);
-      //if the input value is empty - true
-      //false if no value is enterd - first condition
-      //first value is not empty and its equal to [] so true
-      if(!input.value){
-        isEmpty = true;
-        console.log("True = "+ isEmpty);
-      }else if(input.value == "[]"){
-        isArrayEmpty = true;
-        console.log("isArrayEmpty = "+ true);
-      }else{
-        isEmpty = false;
+      
+      if (input.classList.contains("numArrayInput")) {
+        //if input is array
+        if (
+          input.classList.contains("invalid")
+        ) {
+          isArrayEmpty = true;
+        } else {
+          isArrayEmpty = false;
+        }
+      } else {
+        if (input.value == "") {
+          isEmpty = true;
+        } else {
+          isEmpty = false;
+        }
       }
-      console.log("isArrayEmpty = "+ isArrayEmpty +" isEmpty = "+ isEmpty);
-      // if (input.value == "[]") {
-      //   isEmpty = true;
-      //   console.log("isEmpty true");
-      // }else if(input.value != "" && input.value == "[]"){
-      //   isEmpty = true;
-      //   console.log("isEmpty true");
-      // }else{
-      //   isEmpty = false;
-      //   console.log("isEmpty false");
-      // }
     });
 
     if (isEmpty && isArrayEmpty) {
       parentID.querySelector("button").setAttribute("disabled", true);
-    } else if(!isEmpty && isArrayEmpty){
+    } else if (!isEmpty && isArrayEmpty) {
       parentID.querySelector("button").setAttribute("disabled", true);
-      console.log("parentID = "+ parentID);
-      // parentID.querySelector("#span")
-    }else if(isEmpty){
+    } else if (isEmpty) {
       parentID.querySelector("button").setAttribute("disabled", true);
-    }else{
+    } else {
       parentID.querySelector("button").removeAttribute("disabled");
     }
   };
@@ -154,8 +150,7 @@ const validateForCompleteDualArr = (arrItems1, arrItems2, tarVal) => {
     arrItems1[arrItems1.length - 1] == "" &&
     arrItems2[arrItems2.length - 1] == ""
   ) {
-    
-  }else {
+  } else {
     return true;
   }
 };
